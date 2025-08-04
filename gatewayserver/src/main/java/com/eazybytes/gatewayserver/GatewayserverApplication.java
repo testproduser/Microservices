91e826e14c7f930268ee4b160879885791664280
testproduser <vaibhavpatil6242@hotmail.com>
2025-08-04 16:06:24 +0530

    Adding cb to accouts ms
    In gatewayserver , added filter of circuit breaker

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -21,7 +21,8 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/eazybank/accounts/**")
 						.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")))
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p
 						.path("/eazybank/loans/**")
