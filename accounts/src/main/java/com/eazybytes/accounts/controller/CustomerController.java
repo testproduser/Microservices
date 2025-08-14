38ae2d8cdc072db46d217a43730cd1c46e7415d6
testproduser <vaibhavpatil6242@hotmail.com>
2025-08-14 18:14:22 +0530

    updating loggers for tracing demo
    updating loggers for tracing demo

--- accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -59,10 +59,10 @@ public class CustomerController {
                                                                     @RequestParam @Pattern(regexp="(^$|[0-9]{10})",
                                                                             message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+        logger.debug("fetchCustomerDetails method start");
         CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails method end");
         return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
-
     }
 
 
